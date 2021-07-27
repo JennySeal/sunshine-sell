35017aa39cb910db94f8e2af20c512a47ef29efc
Jenny Seal <jennysealft@gmail.com>
2021-07-27 10:57:11 +0100

    remove backslash off server apipoint

--- client/src/config/server.js
+++ client/src/config/server.js
@@ -1,7 +1,7 @@
 
 
 const API_Endpoint = process.env.NODE_ENV === 'production'
-  ? 'https://sunshine-store.herokuapp.com/'
+  ? 'https://sunshine-store.herokuapp.com'
   : 'http://localhost:5500';
 
 export default API_Endpoint;
